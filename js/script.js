2b13b0796ffa1b3676da45991766e7dd4c812aec
Youssef Yasser <Joao1160>
2024-08-19 17:09:11 +0300

    version 1.5

--- js/script.js
+++ js/script.js
@@ -13,17 +13,60 @@ $(document).ready(function () {
     $(".key .close").fadeOut();
   });
 });
-console.log();
 
 // Scroll JS
 
+var homeOffset = $("#landing").offset().top - 50;
+var aboutOffset = $("#biography").offset().top - 50;
+var abilityOffset = $("#ability").offset().top - 50;
+var skillsOffset = $("#skills").offset().top - 50;
+var experienceOffset = $("#experience").offset().top - 50;
+var myProjectsOffset = $("#my-projects").offset().top - 50;
+// var contactOffset = $("#contact").offset().top - 50;
+
 $(window).scroll(function () {
   var windowOffset = $(this).scrollTop();
+  
   if ($(window).width() > 768) {
     if (windowOffset >= $(".biography").offset().top) {
       $(".menu").fadeIn();
     } else {
       $(".menu").fadeOut();
     }
+    
+    if (windowOffset >= homeOffset && windowOffset <= aboutOffset) {
+      console.log("True");
+      $(".menu li a").removeClass("active");
+      $(".menu li:nth-child(2) a").addClass("active");
+    }
+    if (windowOffset >= aboutOffset && windowOffset <= abilityOffset) {
+      $(".menu li a").removeClass("active");
+
+      $(".menu li:nth-child(3) a").addClass("active");
+    }
+    if (windowOffset >= abilityOffset && windowOffset <= skillsOffset) {
+      $(".menu li a").removeClass("active");
+
+      $(".menu li:nth-child(4) a").addClass("active");
+    }
+    if (windowOffset >= skillsOffset && windowOffset <= experienceOffset) {
+      $(".menu li a").removeClass("active");
+
+      $(".menu li:nth-child(5) a").addClass("active");
+    }
+    if (windowOffset >= experienceOffset && windowOffset <= myProjectsOffset) {
+      $(".menu li a").removeClass("active");
+
+      $(".menu li:nth-child(6) a").addClass("active");
+    }
+    if (windowOffset >= myProjectsOffset ) {//&& windowOffset <= contactOffset
+      $(".menu li a").removeClass("active");
+      $(".menu li:nth-child(7) a").addClass("active");
+    }
+    // if (windowOffset >= contactOffset) {
+    //   $(".menu li a").removeClass("active");
+
+    //   $(".menu li:nth-child(6) a").addClass("active");
+    // }
   }
 });
