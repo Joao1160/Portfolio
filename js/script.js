e51286358043cf87c33f7adfea29e800d623b085
Youssef Yasser <Joao1160>
2024-08-21 19:05:52 +0300

    version 1.6

--- js/script.js
+++ js/script.js
@@ -12,6 +12,14 @@ $(document).ready(function () {
     $(".key .open").fadeIn();
     $(".key .close").fadeOut();
   });
+
+  if ($(window).width() < 768) {
+    $(".menu li a").click(function () {
+      $(".menu").fadeOut();
+      $(".key .open").fadeIn();
+      $(".key .close").fadeOut();
+    });
+  }
 });
 
 // Scroll JS
@@ -26,14 +34,15 @@ var myProjectsOffset = $("#my-projects").offset().top - 50;
 
 $(window).scroll(function () {
   var windowOffset = $(this).scrollTop();
-  
+
   if ($(window).width() > 768) {
+
     if (windowOffset >= $(".biography").offset().top) {
       $(".menu").fadeIn();
     } else {
       $(".menu").fadeOut();
     }
-    
+
     if (windowOffset >= homeOffset && windowOffset <= aboutOffset) {
       console.log("True");
       $(".menu li a").removeClass("active");
@@ -59,7 +68,8 @@ $(window).scroll(function () {
 
       $(".menu li:nth-child(6) a").addClass("active");
     }
-    if (windowOffset >= myProjectsOffset ) {//&& windowOffset <= contactOffset
+    if (windowOffset >= myProjectsOffset) {
+      //&& windowOffset <= contactOffset
       $(".menu li a").removeClass("active");
       $(".menu li:nth-child(7) a").addClass("active");
     }
